8e70b5434d2aceb6124210ce274a84e9ba184957
Raigo Tuulik <raigo.tuulik@gmail.com>
2024-12-22 01:00:57 +0200

    Forgot to add controller.ts

--- apps/wrapper/src/movies/movies.controller.ts
+++ apps/wrapper/src/movies/movies.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-import { MoviesService } from './movies.service';
+import { Controller, Get, Query } from '@nestjs/common'
+import { MoviesService } from './movies.service'
+import { MovieResponse, MovieDiscoverParams, MovieSearchParams } from '../types'
 
 @Controller('movies')
 export class MoviesController {
-    constructor(private readonly moviesService: MoviesService) {}
+  constructor(private readonly moviesService: MoviesService) {}
 
-    @Get()
-    async getMovies() {
-        return this.moviesService.getMovies();
-    }
+  // Discover movies
+  @Get('discover')
+  async discoverMovies(@Query('genres') genres?: string): Promise<MovieResponse> {
+    const genreIds = genres ? genres.split(',').map(Number) : []
+    return this.moviesService.discoverMoviesByGenres(genreIds)
+  }
+
+  // Discover with params
+  @Get()
+  async getMovies(@Query() params: Partial<MovieDiscoverParams>): Promise<MovieResponse> {
+    return this.moviesService.getMovies(params)
+  }
+
+  // Search my title
+  @Get('search')
+  async searchMovies(@Query() params: Partial<MovieSearchParams>): Promise<MovieResponse> {
+    return this.moviesService.searchMoviesByTitle(params)
+  }
 }

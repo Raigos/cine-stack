e0ae7a0b84f4daecc7b6e5ffaac7c9a95e4f6cd1
Raigo Tuulik <raigo.tuulik@gmail.com>
2024-12-31 21:06:20 +0200

    Add color mode selector

---
+++ apps/client/src/components/ColorModeSelect.tsx
@@ -0,0 +1,22 @@
+import { Select, MenuItem, SelectProps } from '@mui/material'
+import { useColorScheme } from '@mui/material/styles'
+
+type ColorMode = 'system' | 'light' | 'dark'
+
+export function ColorModeSelect(props: SelectProps) {
+  const { mode, setMode } = useColorScheme()
+
+  if (!mode) return null
+
+  return (
+      <Select
+          value={mode}
+          onChange={(event) => setMode(event.target.value as ColorMode)}
+          {...props}
+      >
+        <MenuItem value="system">System</MenuItem>
+        <MenuItem value="light">Light</MenuItem>
+        <MenuItem value="dark">Dark</MenuItem>
+      </Select>
+  )
+}
